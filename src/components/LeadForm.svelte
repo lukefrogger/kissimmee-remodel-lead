0d4a2a8484315fb6354139aeb8513a37b8108e7f
Luke Frauhigher <lukefrogger@gmail.com>
2019-10-08 22:37:28 -0600

    first

---
+++ src/components/LeadForm.svelte
@@ -0,0 +1,68 @@
+<script >
+    import { fly, fade } from 'svelte/transition';
+    import querystring from "query-string";
+    let showModal = false;
+
+   async function submitForm(formData) {
+
+        var postData = querystring.stringify({
+            'email': formData.target.elements.email.value,
+            'firstname': formData.target.elements.firstName.value,
+            'lastname': formData.target.elements.lastName.value,
+            'phone':  formData.target.elements.phone.value
+        });
+        
+        let resp = await fetch('https://forms.hubspot.com/uploads/form/v2/6498274/d9262076-1c0e-4fa3-bea2-24436765073c', {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: postData
+        });
+
+        if(resp.status == 0 || resp.status == 204) {
+            showModal = true;
+        }
+        setTimeout(() => document.location.reload(), 2000)
+   }
+</script>
+
+<div class="form ">
+    <div class="has-text-centered">
+        <h3 class="is-size-4 field">Carmel Home Remodeling</h3>
+        <p class="has-text-grey	">Fill out the form below to recieve your $250 coupon. Someone will be in contact with you to quote your project. Only 6 coupons left!</p>
+        <br>
+    </div>
+    <form on:submit|preventDefault={submitForm}>
+        <div class="field">
+            <label>First Name</label>
+            <input class="input" name="firstName" type="text"  required/>
+        </div>
+        <div class="field">
+            <label>Last Name</label>
+            <input class="input" name="lastName" type="text" required/>
+        </div>
+        <div class="field">
+            <label>Email</label>
+            <input class="input" name="email" type="email" required/>
+        </div>
+        <div class="field">
+            <label>Phone Number</label>
+            <input class="input" name="phone" type="text" required/>
+        </div>
+        <button class="button is-primary"  type="submit">Get A Quote!</button>
+    </form>
+</div>
+
+
+{#if showModal}
+<div class="modal is-active" transition:fade>
+  <div class="modal-background" transition:fade></div>
+  <div class="modal-content has-text-centered" transition:fade>
+    <div class="box">
+        <h4 class="is-size-4">Thank you for submitting the form!</h4>
+    </div>
+  </div>
+</div>
+{/if}
---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+	import LeadForm from '../components/LeadForm.svelte';
+</script>
+
+<style>
+	#form {
+		display: flex;
+		justify-content: center;
+		margin-top: 20px;
+	}
+</style>
+
+<div class="columns" id="form">
+	<div class="column sis-one-third-tablet is-one-quarter-desktop">
+		<div class="box">
+			<LeadForm />
+		</div>
+	</div>
+</div>
+	
